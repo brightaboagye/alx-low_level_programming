956c8729a1b6a41dabbce1dbe9c79b6a4a0ad34c
Bright Aboagye <brightkotos123@gmail.com>
2023-06-18 11:20:11 -0700

    Hello world

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,15 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
- * main - Entry point
+ * main - Entry point of the program
+ *
+ * Description: This function uses the puts function to print out
+ * the specified string with a newline.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art, \n");
+	/* The puts function prints out the string with a newline */
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
