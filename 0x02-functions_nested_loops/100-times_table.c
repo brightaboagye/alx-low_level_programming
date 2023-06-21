5f48635f88497d40516c76d5318e418d286a0f15
Bright Aboagye <brightkotos123@gmail.com>
2023-06-20 17:35:48 -0700

    C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * print_times_table - Prints the n times table, starting with 0.
+ * @n: The number up to which the times table is printed.
+ */
+void print_times_table(int n)
+{
+	int i, j, result;
+
+	if (n >= 0 && n <= 15)
+	{
+		for (i = 0; i <= n; i++)
+		{
+			for (j = 0; j <= n; j++)
+			{
+				result = i * j;
+				if (j != 0)
+					printf(", ");
+				printf("%-3d", result);
+			}
+			printf("\n");
+		}
+	}
+}
---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Computes and returns the last digit of a number.
+ * @number: The number from which to extract the last digit.
+ *
+ * Return: The last digit of the given number.
+ */
+int print_last_digit(int number)
+{
+	int last = number % 10;
+
+	if (last < 0)
+	{
+		last = -last;
+	}
+	_putchar(last + '0');
+	return (last);
+}
