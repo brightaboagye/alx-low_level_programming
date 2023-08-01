f9c374e4abc1d2a3bc4b5f58964a137d1f69613f
Bright Aboagye <brightkotos123@gmail.com>
2023-08-01 14:25:43 -0700

    C - More singly linked lists

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - Function that free memomry allocated to a node in list
+ * @head: paremeter of poitner to node
+ */
+void free_listint2(listint_t **head)
+{
+
+	listint_t *temp;
+
+	if (head == NULL)
+	{
+		return;
+	}
+
+	while (*head != NULL)
+	{
+		temp = (*head)->next;
+		free(*head);
+		*head = temp;
+	}
+
+}
---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+/**
+ * sum_listint - Entry point of sum function
+ * @head: Params of pointer to node
+ *
+ * Return: returns int of sum node data;
+ */
+
+int sum_listint(listint_t *head)
+{
+	listint_t *temp;
+	int sum;
+
+	temp = head;
+	sum = 0;
+
+	if (head == NULL)
+	{
+		return (0);
+	}
+
+	while (temp != NULL)
+	{
+		sum += temp->n;
+		temp = temp->next;
+	}
+
+	return (sum);
+}
