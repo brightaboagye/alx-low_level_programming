ae72177b82fce2967b164b2ebf0ea70109c3d658
Bright Aboagye <brightkotos123@gmail.com>
2023-08-07 11:19:59 -0700

    C - File I/O

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * read_textfile - entry point of read file program
+ * @letters: letters is the number of letters it should read and print
+ * @filename: filname
+ * Return: return an unsigned int of the number of letters
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *buf;
+	ssize_t fd;
+	ssize_t w;
+	ssize_t t;
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	buf = malloc(sizeof(char) * letters);
+	t = read(fd, buf, letters);
+	w = write(STDOUT_FILENO, buf, t);
+
+	free(buf);
+	close(fd);
+	return (w);
+}
